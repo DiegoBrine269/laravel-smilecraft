475e81273b2c5319c117e75ed54b5e37ac1df148
Diego Oloarte <64753503+DiegoBrine269@users.noreply.github.com>
2023-10-21 17:43:46 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -54,4 +54,4 @@ Route::post('login', [AuthController::class, 'login']);
 
 
 
-// Route::post('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'register']);
